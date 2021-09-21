44442b9fc663f6196f5ab3e0fe16c75ce21dad08
thanianababan <thaniadwiaprilianababan@gmail.com>
2021-09-21 14:35:16 +0700

    commit

---
+++ application/views/v_about.php
@@ -0,0 +1,8 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Halaman About <br>
+        Nama : Thania Nababan <br>
+        Alamat : Tozai Baru, Kota Pematangsiantar
+    </p>
+</section>
